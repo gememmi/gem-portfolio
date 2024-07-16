3c64fd72f33f4c61d2830ffc3095369b35bd9660
Emily <emelizadaniels@gmail.com>
2024-07-16 12:13:21 -0400

    about page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,13 +2,13 @@ import { Link } from 'react-router-dom';
 
 export default function About() {
   return (
-    <>
-    <button><Link to="/"> ← </Link></button>
     <div id="about-div">
-      Emily is a web developer and STEAM educator currently residing in
+    <button><Link to="/"> ← </Link></button>
+    <div id="about-text">
+      <p>      Emily is a web developer and STEAM educator currently residing in
       Philadelpiha, PA. She is available for free-lance work and can be
-      contacted <Link to="/contact">Contact</Link>
+      contacted <Link to="/contact">here</Link> ← </p>
     </div>
-    </>
+    </div>
   );
 }
