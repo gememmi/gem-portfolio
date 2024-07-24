9b3f165791d714ceee25b816e9377d52964f0426
Emily <emelizadaniels@gmail.com>
2024-07-24 14:52:23 -0400

    grid

--- src/components/ProjectContainer.jsx
+++ src/components/ProjectContainer.jsx
@@ -1,40 +1,70 @@
-import { Link } from 'react-router-dom';
-import ProjectShow from './ProjectShow';
+import { Link } from "react-router-dom";
+import ProjectShow from "./ProjectShow";
 
+export default function ProjectContainer() {
+  const projectsArray = [
+    {
+      id: 1,
+      name: "StarBright",
+      demo: "https://www.loom.com/share/8a255f28f87c47918d30dd16102bba52",
+      github: "https://github.com/gememmi/StarBright",
+      image: "./moon 1.png"
+    },
+    {
+      id: 2,
+      name: "Project 2",
+      demo: "https://www.facebook.com/",
+       github: "https://github.com",
+       image: "./moon 1.png"
 
-export default function ProjectContainer(){
+    },
+    {
+      id: 3,
+      name: "Project 3",
+      demo: "https://www.facebook.com/",
+       github: "https://github.com",
+       image: "./moon 1.png"
+    },
+    // {
+    //   id: 4,
+    //   name: "Project 4",
+    //   demo: "https://www.facebook.com/",
+    //    github: "https://github.com",
+    //    image: "./moon 1.png"
+    // },
+    // {
+    //   id: 5,
+    //   name: "Project 4",
+    //   demo: "https://www.facebook.com/",
+    //    github: "https://github.com",
+    //    image: "./moon 1.png"
+    // },
+    // {
+    //   id: 6,
+    //   name: "Project 4",
+    //   demo: "https://www.facebook.com/",
+    //    github: "https://github.com",
+    //    image: "./moon 1.png"
+    // },
+    // {
+    //   id: 7,
+    //   name: "Project 4",
+    //   demo: "https://www.facebook.com/",
+    //    github: "https://github.com",
+    //    image: "./moon 1.png"
+    // },
+  ];
 
-    const projectsArray =[
-        {
-            id:1,
-            name:"Project 1",
-            demo: "https://www.facebook.com/"
-        },
-        {
-            id:2,
-            name:"Project 2",
-            demo: "https://www.facebook.com/"
-    
-        },
-        {
-            id:3,
-            name:"Project 3",
-            demo: "https://www.facebook.com/"
-        },
-        {
-            id:4,
-            name:"Project 4",
-            demo: "https://www.facebook.com/"
-        },
-    
-    ]
-
-    return(
-        <div id="project-div">
-            <button><Link to="/"> ← </Link></button>
-            {projectsArray.map((project) => (
-                <ProjectShow key={project.id} project={project}/>
-            ))}
-        </div>
-    )
+  return (
+    <div id="project-div">
+      <button className="button">
+        <Link to="/" id="back-btn"> ← </Link>
+      </button>
+      <div id="project-library">
+        {projectsArray.map((project) => (
+          <ProjectShow key={project.id} project={project} />
+        ))}
+      </div>
+    </div>
+  );
 }
--- src/components/ProjectShow.jsx
+++ src/components/ProjectShow.jsx
@@ -1,11 +1,21 @@
-export default function ProjectShow({ project }){
-
-
-
-    return (
-        <div>
-            <h3>{project.name}</h3>
-            <a href={project.demo} target="_blank" rel="noopener noreferrer">Demo</a>
-        </div>
-    );
+export default function ProjectShow({ project }) {
+  return (
+    <div className="project">
+      <h4>{project.name}</h4>
+      <img alt="project image here"></img>
+      <div id="project-buttons">
+        <button>
+          {"Demo"}
+          <a href={project.demo} target="_blank" rel="noopener noreferrer">
+          </a>
+        </button>
+        <button>
+          {" Github"}
+          <a href={project.github} target="_blank" rel="noopener noreferrer">
+            
+          </a>
+        </button>
+      </div>
+    </div>
+  );
 }
