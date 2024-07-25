05f3fa9d8be96cfbc1ca9ed7e7d63f1e2c347b7e
Emily <emelizadaniels@gmail.com>
2024-07-25 16:52:38 -0400

    added metadata

--- src/components/ProjectContainer.jsx
+++ src/components/ProjectContainer.jsx
@@ -5,10 +5,10 @@ export default function ProjectContainer() {
   const projectsArray = [
     {
       id: 1,
-      name: "Project 4",
-      demo: "https://www.facebook.com/",
-       github: "https://github.com",
-       image: "./moon 1.png"
+      name: "Kasia's Catering Site",
+      demo: "https://kasialemanek.netlify.app/",
+       github: "https://github.com/gememmi/kasia-site-1.0",
+       image: "./kasiasite.png"
     },
     {
 
@@ -22,30 +22,30 @@ export default function ProjectContainer() {
     {
       id: 3,
       name: "Rome",
-      demo: "https://www.facebook.com/",
-       github: "https://github.com",
+      demo: "https://www.loom.com/share/43bc1bbc37a04093ab0e085206a40735",
+       github: "https://github.com/gememmi/Rome",
        image: "./rome.png"
 
     },
     {
       id: 4,
       name: "Journey Back to Flatiron",
-      demo: "https://www.facebook.com/",
-       github: "https://github.com",
+      demo: "https://www.loom.com/share/1c85e7c990e7429eb0c78705f3a813c8",
+       github: "https://github.com/gememmi/Journey-Back-to-Flatiron",
        image: "./journeytoflatiron.png"
     },
     {
       id: 5,
       name: "Aaron and Emily's Bookshop",
-      demo: "https://www.facebook.com/",
-       github: "https://github.com",
+      demo: "https://www.loom.com/share/8af0d2232ec747a586a822040a8f4c86?sid=31d736c7-d1dd-40ff-9ec7-e5aa2ec3a086",
+       github: "https://github.com/gememmi/AaronEmilyBookshop",
        image: "./aaronandemilybookshopt.png"
     },
     {
       id: 6,
-      name: "Project 4",
-      demo: "https://www.facebook.com/",
-       github: "https://github.com",
+      name: "5 o'Clock Somewhere!",
+      demo: "https://www.loom.com/share/3c983c68eb8747258fc1e75f09422141?sid=d0b39b93-7245-4686-8539-5d07c32bbf64",
+       github: "https://github.com/gememmi/5oClockSomewhere",
        image: "./5oclocksomewhere.png"
     },
   ];
