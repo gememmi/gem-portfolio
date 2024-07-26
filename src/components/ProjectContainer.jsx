add3200928cb0bb48d55f72bd7a100fd3161b827
Emily <emelizadaniels@gmail.com>
2024-07-26 12:48:56 -0400

    library is good at 100

--- src/components/ProjectContainer.jsx
+++ src/components/ProjectContainer.jsx
@@ -5,55 +5,61 @@ export default function ProjectContainer() {
   const projectsArray = [
     {
       id: 1,
-      name: "Kasia's Catering Site",
+      name: "Kasia's Catering",
       demo: "https://kasialemanek.netlify.app/",
-       github: "https://github.com/gememmi/kasia-site-1.0",
-       image: "./kasiasite.png"
+      github: "https://github.com/gememmi/kasia-site-1.0",
+      image: "./kasiasite.png",
+      alt: "black catering site screenshot"
     },
     {
-
-      
       id: 2,
       name: "StarBright",
       demo: "https://www.loom.com/share/8a255f28f87c47918d30dd16102bba52",
       github: "https://github.com/gememmi/StarBright",
-      image: "./starbright.png"
+      image: "./starbright.png",
+      alt: "pink and purple log in screen to mood tracker"
     },
     {
       id: 3,
       name: "Rome",
       demo: "https://www.loom.com/share/43bc1bbc37a04093ab0e085206a40735",
-       github: "https://github.com/gememmi/Rome",
-       image: "./rome.png"
-
+      github: "https://github.com/gememmi/Rome",
+      image: "./rome.png",
+      alt:"white and black forum screenshot"
     },
     {
       id: 4,
       name: "Journey Back to Flatiron",
       demo: "https://www.loom.com/share/1c85e7c990e7429eb0c78705f3a813c8",
-       github: "https://github.com/gememmi/Journey-Back-to-Flatiron",
-       image: "./journeytoflatiron.png"
+      github: "https://github.com/gememmi/Journey-Back-to-Flatiron",
+      image: "./journeytoflatiron.png",
+      alt:"black videogame start screenshot"
     },
     {
       id: 5,
       name: "Aaron and Emily's Bookshop",
       demo: "https://www.loom.com/share/8af0d2232ec747a586a822040a8f4c86?sid=31d736c7-d1dd-40ff-9ec7-e5aa2ec3a086",
-       github: "https://github.com/gememmi/AaronEmilyBookshop",
-       image: "./aaronandemilybookshopt.png"
+      github: "https://github.com/gememmi/AaronEmilyBookshop",
+      image: "./aaronandemilybookshopt.png",
+      alt: "red online bookshop screenshot"
     },
     {
       id: 6,
       name: "5 o'Clock Somewhere!",
       demo: "https://www.loom.com/share/3c983c68eb8747258fc1e75f09422141?sid=d0b39b93-7245-4686-8539-5d07c32bbf64",
-       github: "https://github.com/gememmi/5oClockSomewhere",
-       image: "./5oclocksomewhere.png"
+      github: "https://github.com/gememmi/5oClockSomewhere",
+      image: "./5oclocksomewhere.png",
+      alt: "nyc beer index screenshot"
     },
   ];
 
   return (
     <div id="project-div">
       <button className="button">
-        <Link to="/" id="back-btn"> ← </Link>
+        <Link to="/" id="back-btn">
+          {" "}
+          ←{" "}
+        </Link>
       </button>
       <div id="project-library">
         {projectsArray.map((project) => (
--- src/components/ProjectShow.jsx
+++ src/components/ProjectShow.jsx
@@ -2,18 +2,21 @@ export default function ProjectShow({ project }) {
   return (
     <div className="project">
       <h4>{project.name}</h4>
-      <img alt="project image here"></img>
+      <div className="project-image-div">
+        <img alt={project.alt} src={project.image}></img>
+      </div>
       <div id="project-buttons">
         <button>
           {"Demo"}
-          <a href={project.demo} target="_blank" rel="noopener noreferrer">
-          </a>
+          <a href={project.demo} target="_blank" rel="noopener noreferrer"></a>
         </button>
         <button>
           {" Github"}
-          <a href={project.github} target="_blank" rel="noopener noreferrer">
-            
-          </a>
+          <a
+            href={project.github}
+            target="_blank"
+            rel="noopener noreferrer"
+          ></a>
         </button>
       </div>
     </div>
