2b8f9a1bffa03291b212aa4ba5a4c7d7aa403049
Emily <emelizadaniels@gmail.com>
2024-07-29 11:53:38 -0400

    may have resolved iamges

--- src/components/ProjectContainer.jsx
+++ src/components/ProjectContainer.jsx
@@ -1,5 +1,11 @@
 import { Link } from "react-router-dom";
 import ProjectShow from "./ProjectShow";
+import kasia from "../images/kasiasite.png";
+import starbright from "../images/starbright.png";
+import rome from "../images/rome.png";
+import aaronemily from '../images/aaronandemilybookshopt.png';
+import journey from '../images/journeytoflatiron.png';
+import oclock from '../images/5oclocksomewhere.png'
 
 export default function ProjectContainer() {
   const projectsArray = [
@@ -8,7 +14,7 @@ export default function ProjectContainer() {
       name: "Kasia's Catering",
       demo: "https://kasialemanek.netlify.app/",
       github: "https://github.com/gememmi/kasia-site-1.0",
-      image: "./images/kasiasite.png",
+      image: kasia,
       alt: "black catering site screenshot"
     },
     {
@@ -16,7 +22,7 @@ export default function ProjectContainer() {
       name: "StarBright",
       demo: "https://www.loom.com/share/8a255f28f87c47918d30dd16102bba52",
       github: "https://github.com/gememmi/StarBright",
-      image: "./images/starbright.png",
+      image: starbright,
       alt: "pink and purple log in screen to mood tracker"
     },
     {
@@ -24,7 +30,7 @@ export default function ProjectContainer() {
       name: "Rome",
       demo: "https://www.loom.com/share/43bc1bbc37a04093ab0e085206a40735",
       github: "https://github.com/gememmi/Rome",
-      image: "./images/rome.png",
+      image: rome,
       alt:"white and black forum screenshot"
     },
     {
@@ -32,7 +38,7 @@ export default function ProjectContainer() {
       name: "Journey Back to Flatiron",
       demo: "https://www.loom.com/share/1c85e7c990e7429eb0c78705f3a813c8",
       github: "https://github.com/gememmi/Journey-Back-to-Flatiron",
-      image: "./images/journeytoflatiron.png",
+      image: journey,
       alt:"black videogame start screenshot"
     },
     {
@@ -40,7 +46,7 @@ export default function ProjectContainer() {
       name: "Aaron and Emily's Bookshop",
       demo: "https://www.loom.com/share/8af0d2232ec747a586a822040a8f4c86?sid=31d736c7-d1dd-40ff-9ec7-e5aa2ec3a086",
       github: "https://github.com/gememmi/AaronEmilyBookshop",
-      image: "./images/aaronandemilybookshopt.png",
+      image: aaronemily,
       alt: "red online bookshop screenshot"
     },
     {
@@ -48,7 +54,7 @@ export default function ProjectContainer() {
       name: "5 o'Clock Somewhere!",
       demo: "https://www.loom.com/share/3c983c68eb8747258fc1e75f09422141?sid=d0b39b93-7245-4686-8539-5d07c32bbf64",
       github: "https://github.com/gememmi/5oClockSomewhere",
-      image: "./images/5oclocksomewhere.png",
+      image: oclock,
       alt: "nyc beer index screenshot"
     },
   ];
