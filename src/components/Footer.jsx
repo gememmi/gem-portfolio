ad46b8ef5f1425b21525f64ffd2cbdb501e844d3
Emily <emelizadaniels@gmail.com>
2024-07-29 12:07:53 -0400

    adding logos

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,9 @@
+import emilylogo from '../images/Emily-Logo-Inverse.png';
+
 export default function Footer(){
     return(
         <div id="footer-div">
-            <p>Designed and Developed by Emily Daniels </p> <img src="./images/Emily-Logo.png" alt="Emily Daniels Logo green and blue"/>
+            <p>Designed and Developed by Emily Daniels </p> <img src={emilylogo} alt="Emily Daniels Logo green and blue"/>
         </div>
     )
 }
